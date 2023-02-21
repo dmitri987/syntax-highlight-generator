8bbc622494682ff10c721fbdf939f06ec016fc77
Dmytro <shorop.d@gmail.com>
2023-02-21 14:39:01 +0200

    chore: format 'postcss.config.cjs'

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,8 @@
 module.exports = {
-  sourceType: 'module',
+  sourceType: "module",
   plugins: {
-    'postcss-import': {},
+    "postcss-import": {},
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
